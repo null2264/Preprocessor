5e0b005105f607d823fa7d9b6938bd590acc2986
Deftu <deftudev@gmail.com>
2023-06-04 20:00:29 +0200

    Print version

--- src/main/kotlin/com/replaymod/gradle/preprocess/RootPreprocessPlugin.kt
+++ src/main/kotlin/com/replaymod/gradle/preprocess/RootPreprocessPlugin.kt
@@ -5,7 +5,13 @@ import org.gradle.api.Project
 import org.gradle.kotlin.dsl.create
 
 class RootPreprocessPlugin : Plugin<Project> {
+    val version: String
+        get() {
+            return javaClass.`package`.implementationVersion ?: "0.0.0"
+        }
+
     override fun apply(project: Project) {
+        project.logger.lifecycle("Processor $version")
         project.extensions.create("preprocess", RootPreprocessExtension::class)
     }
 }
