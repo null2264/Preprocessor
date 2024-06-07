dbdc3a925cbc066f0f2816ec1267db88786e4d2d
Deftu <deftudev@gmail.com>
2024-06-08 00:59:12 +0200

    Update Remap

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,7 +42,7 @@ repositories {
 dependencies {
     implementation(gradleApi())
     implementation(localGroovy())
-    implementation("dev.deftu:Remap:0.2.0")
+    implementation("dev.deftu:Remap:0.2.1")
     implementation("net.fabricmc:tiny-mappings-parser:0.2.1.13")
     testImplementation("io.kotest:kotest-runner-junit5-jvm:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-core-jvm:$kotestVersion")
